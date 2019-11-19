f765fd1f07082d9b07ad0f8dbdef7ee33e49cefd
JobJoy <57930527+JobJoy@users.noreply.github.com>
2019-11-18 20:09:13 -0800

    you can load your program !!

--- src/qvm.c
+++ src/qvm.c
@@ -1,2 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+int main(int argc, char** argv)
+{
+    FILE *file = fopen(argv[1], "rb");
+    if (file == NULL)
+    {
+        printf("Unable to load file.\n");
+    }
+    else
+    {
+        printf("Loaded successfully.\n");
+    }
+}
