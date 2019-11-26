7dc6b3bf0adb6b74ada588b8f148a9af0d2a6d0c
JobJoy <57930527+JobJoy@users.noreply.github.com>
2019-11-25 18:29:33 -0800

    fixed about program counter

--- src/cpu.h
+++ src/cpu.h
@@ -4,9 +4,17 @@ void fetch(unsigned char *memory, int *_PC)
 {
     int PC;
     printf("Fetching instructions...\n");
-    for(PC = *_PC; PC < 0xFFFF; PC = PC + 1)
+    for(PC = *_PC; PC < 0xFFFF; PC = PC + 2)
     {
-        unsigned char byte = memory[PC];
-        printf("PC: %x > %.2x\n",PC, byte);
+        unsigned char Hbyte = memory[PC];
+        unsigned char Lbyte = memory[PC+1];
+        unsigned opcode = (Hbyte << 8) | Lbyte;
+        switch(opcode)
+        {
+            case 0x00:
+                printf("Clear screen.\n");
+                break;
+        }
+        printf("PC: %x > 0x%.4x\n",PC, opcode);
     }
 }
