52851a3495d9717e64cf1c71ff5ce9b4c92dd5d6
Flammableobject <58024821+Flammableobject@users.noreply.github.com>
2019-12-23 16:17:40 +0700

    Update till ox32XY
    Fuck

--- src/cpu.h
+++ src/cpu.h
@@ -12,21 +12,32 @@ void fetch(unsigned char *memory, int *_PC)
         switch(opcode)
         {
             case 0x0000:
-                printf("Clear screen.\n");
+                printf("Clears the display.");
                 break;
             case 0x0001:
-                printf("Returns from a subroutine.\n");
+                printf("Returns from a subroutine. Sets the PC to address at the top of the stack, then subtracts 1 from the stack pointer (SP).\n");
                 break;
             case 0x0002:
-                printf("goto NNNN;.\n");
+                printf("Jumps to address NNNN(High byte from R0 register and low byte from R1 register). Sets the PC to NNNN.\n");
                 break;
             case 0x0003:
-                printf("*(0xNNNN)();.\n");
+                printf("Calls subroutine at NNNN(High byte from R0 register and low byte from R1 register). Increments the stack pointer, then puts 
+the current PC on the top of the stack. The PC is then set to NNNN.\n");
                 break;
             case 0x1XNN:
-                printf("RX = NN;.\n"):
+                printf("Sets RX to NN.\n");
                 break;
-             
+            case 0x2XY0:
+                printf("Sets RX to the value of RY.\n");
+                break;
+            case 0x2NN1:
+                printf("Set (R0<<8) | R1 memory location to NN.\n");
+                break;
+            case 0x31XY:
+                printf("Sets RX to RX or RY (Bitwise OR Operation).\n");
+                break;
+            case 0x32XY:
+                printf("Sets RX to RX and RY (Bitwise AND Operation)\n").
                        
         }
         printf("PC: %x > 0x%.4x\n",PC, opcode);
